f81d4ed1da281378c09d98a6ba19258ef48963ec
KartikSGulkotwar <114782932+KartikSGulkotwar@users.noreply.github.com>
2024-02-13 04:35:49 +0000

    commit 1.2

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC=gcc
-CFLAGS=-Wall -Wextra
-TARGET=search
+CFLAGS=-Wall -g
+TARGET=kgulkotw_HW02
 
+# Default build target
 all: $(TARGET)
 
+# Rule to build the target executable
 $(TARGET): kgulkotw_HW02.c
-	$(CC) $(CFLAGS) kgulkotw_HW02.c -o $(TARGET)
+	$(CC) $(CFLAGS) -o $(TARGET) kgulkotw_HW02.c
 
+# Clean the built files
 clean:
 	rm -f $(TARGET)
 
--- kgulkotw_HW02.c
+++ kgulkotw_HW02.c
@@ -5,79 +5,129 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
-// Defining a struct here to hold program options
 typedef struct {
-    int verbose_;
-    long file_Size;
-    char *search_String;
-    int depth;
-    char fileType; // 'f' for files, 'd' for directories, 0 if they are not set
+    int verbose;          // -v
+    long minSize;         // -L
+    char *searchString;   // -s
+    int searchDepth;      // depth for -s
+    char fileType;        // -t, 'f' for files, 'd' for directories
 } Options;
 
-Options opts = {0, -1, NULL, -1, 0}; // Initializing all the options to default values
-void parse_Options(int argc, char *argv[]) {
+Options opts = {0, -1, NULL, -1, 0};
+
+void parseOptions(int argc, char *argv[]) {
     int opt;
     while ((opt = getopt(argc, argv, "vL:s:t:")) != -1) {
         switch (opt) {
             case 'v':
-                opts.verbose_ = 1;
+                opts.verbose = 1;
                 break;
             case 'L':
-                opts.file_Size = atol(optarg);
+                opts.minSize = atol(optarg);
                 break;
             case 's':
-                opts.search_String = optarg;
-                if (optind < argc && *argv[optind] != '-') {
-                    opts.depth = atoi(argv[optind]);
+                opts.searchString = optarg;
+                opts.searchDepth = 0; // Default depth
+                // Check for the next argument to be a depth, if it exists and is a number
+                if (optind < argc && argv[optind] != NULL && isdigit(argv[optind][0])) {
+                    opts.searchDepth = atoi(argv[optind]);
                     optind++;
                 }
                 break;
             case 't':
                 opts.fileType = optarg[0];
                 break;
-            default: /* '?' */
-                fprintf(stderr, "Usage: %s [-v] [-L size] [-s string depth] [-t f|d] [directory]\n", argv[0]);
+            default:
+                fprintf(stderr, "Usage: %s [-v] [-L minSize] [-s searchString [depth]] [-t f|d] [directory]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
 }
-void list_Files(const char *startDir, int currentDepth) {
-    DIR *dir;
+
+
+void printFileInfo(struct stat *statbuf, const char *name, const char *linkTarget, int depth, char isDirectory) {
+    // Generate indentation based on depth
+    char indent[256] = {0};
+    for (int i = 0; i < depth && i < sizeof(indent) - 1; i++) {
+        strcat(indent, "\t"); // Use "\t" for tabs or "  " for spaces
+    }
+
+    long size = isDirectory ? 0 : statbuf->st_size;
+    if (opts.verbose) {
+        printf("%s%s // Size: %ld bytes, Permissions: %o, Last Access: %s\n",
+               indent, name, size, statbuf->st_mode & 0777,
+               ctime(&statbuf->st_atime));
+    } else {
+        printf("%s%s%s\n", indent, name, linkTarget ? linkTarget : "");
+    }
+}
+
+void traverse(const char *dirPath, int currentDepth) {
+    DIR *dir = opendir(dirPath);
     struct dirent *entry;
     struct stat statbuf;
-    char path[1024];
+    char fullPath[PATH_MAX];
 
-    if (!(dir = opendir(startDir))) {
+    if (!dir) {
         perror("opendir");
         return;
     }
 
+    char indent[256] = {0};
+    for (int i = 0; i < currentDepth && i < sizeof(indent) - 1; i++) {
+        strcat(indent, "\t"); // Use "\t" for tabs or "  " for spaces
+    }
+
     while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-            continue;
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
 
-        snprintf(path, sizeof(path), "%s/%s", startDir, entry->d_name);
-        lstat(path, &statbuf);
 
-        
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", dirPath, entry->d_name);
+        lstat(fullPath, &statbuf);
 
-        printf("%s\n", path); 
+        // Apply -L size filter
+        if (opts.minSize != -1 && statbuf.st_size < opts.minSize) continue;
 
-        if (S_ISDIR(statbuf.st_mode)) {
-            list_Files(path, currentDepth + 1); 
+        // Apply -s search string and depth filter
+        if (opts.searchString != NULL && (strstr(entry->d_name, opts.searchString) == NULL || currentDepth > opts.searchDepth)) continue;
+
+        // Determine if the entry is a directory or a regular file
+        int isDirectory = S_ISDIR(statbuf.st_mode);
+
+        // Apply -t file type filter
+        if ((opts.fileType == 'f' && !S_ISREG(statbuf.st_mode)) || (opts.fileType == 'd' && !isDirectory)) continue;
+
+        // Print directories or files based on the verbose flag and fileType
+        if (isDirectory) {
+            if (opts.verbose || opts.fileType == 'd') {
+                printFileInfo(&statbuf, entry->d_name, NULL, currentDepth, 1);
+            }
+            if (currentDepth < opts.searchDepth || opts.searchDepth == -1) {
+                // printf("%s%s:\n", indent, entry->d_name); // Print the directory name with indentation
+                traverse(fullPath, currentDepth + 1); // Recurse into the directory
+            }
+        } else {
+            // It's a file, check if we're listing files
+            if (opts.fileType != 'd') {
+                printFileInfo(&statbuf, entry->d_name, NULL, currentDepth, 0);
+            }
         }
     }
+
     closedir(dir);
 }
-int main(int argc, char *argv[]) {
-    parse_Options(argc, argv);
 
-    const char *startDir = ".";
+
+int main(int argc, char *argv[]) {
+    parseOptions(argc, argv);
+
+    char *startDir = ".";
     if (argc > optind) {
         startDir = argv[optind];
     }
 
-    list_Files(startDir, 0);
+    traverse(startDir, 0);
     return 0;
 }
