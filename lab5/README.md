b13d258be3c464ad22e1c4c7fd22d191fafec66f
KartikSGulkotwar <114782932+KartikSGulkotwar@users.noreply.github.com>
2024-02-15 02:02:17 +0000

    Commit t

---
+++ lab5/README.md
@@ -0,0 +1,7 @@
+C program to extend the readdir program to traverse the file hierarchy recursively and list all the sub directories and the files in these sub directories.
+Recursive Traversal: The utility navigates through all levels of the directory structure from the specified start point, listing all directories and files.
+Item Counting: Each item within a directory is numbered to provide a clearer context of the directory contents.
+### Compilation
+gcc -o  traverse lab5.c
+
+Link for Repository:
--- lab5/lab5.c
+++ lab5/lab5.c
@@ -3,7 +3,7 @@
 #include <dirent.h>
 #include <string.h>
 
-void traverseDirectory(char *dirname, int level);
+void traveseDirectory_(char *dirname, int level);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -11,12 +11,12 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-    traverseDirectory(argv[1], 0);
+    traveseDirectory_(argv[1], 0);
 
     return 0;
 }
 
-void traverseDirectory(char *dirname, int level) {
+void traveseDirectory_(char *dirname, int level) {
     DIR *dir;
     struct dirent *dirent;
 
@@ -30,20 +30,20 @@ void traverseDirectory(char *dirname, int level) {
     while ((dirent = readdir(dir)) != NULL) {
         if (strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0) {
             for (int i = 0; i < level; i++) {
-                printf("  "); // Indent based on the directory level
+                printf("  "); 
             }
 
             printf("[%d] %s\n", count, dirent->d_name);
             count++;
 
-            // Check if the entry is a directory and traverse it recursively
+            
             if (dirent->d_type == DT_DIR) {
-                // Create a new path for the subdirectory
+                
                 char path[PATH_MAX];
                 snprintf(path, sizeof(path), "%s/%s", dirname, dirent->d_name);
 
-                // Recursively traverse the subdirectory
-                traverseDirectory(path, level + 1);
+                
+                traveseDirectory_(path, level + 1);
             }
         }
     }
